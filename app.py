3e8d2069ec7dfb6b4da90e171037e1032e1c44d3
omvignesh2010 <oomvignesh@gmail.com>
2025-07-11 10:02:53 +0530

    app.py

--- app.py
+++ app.py
@@ -11,8 +11,28 @@ def login():
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    data = request.json
-    return jsonify({"status": "Signup success", "user": data.get("username")})
+    def signup():
+    data = request.get_json()
+    name = data.get('fullName')
+    email = data.get('email')
+    password = data.get('password')
+
+    if not name or not email or not password:
+        return jsonify({"message": "Missing fields"}), 400
+
+    if not is_strong_password(password):
+        return jsonify({"message": "Weak password"}), 400
+
+    if collection.find_one({"email": email}):
+        return jsonify({"message": "User already exists"}), 409
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    collection.insert_one({
+        "name": name,
+        "email": email,
+        "password": hashed_password.decode('utf-8')
+    })
 
 if __name__ == "__main__":
     app.run()
